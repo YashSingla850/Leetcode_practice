539fba6e8ff6dbfddd40b3633b2f08ce2153b93f
yashgupta9810 <yashg0621@gmail.com>
2022-01-23 11:15:14 +0530

    commit

---
+++ leetcode/ListNode4.java
@@ -0,0 +1,23 @@
+// class Solution {
+//     Boolean[] dp = new Boolean[100000+1];
+//     public boolean winnerSquareGame(int n) {
+//         if (dp[n]!=null){
+
+//         return dp[n];
+//         }
+//         Boolean aliceWin = false;
+//         for(int move =1;n>=move*move ; move++){
+
+//         if(n-move*move==0){
+//             aliceWin = true;
+//             break;
+//         }else{
+
+//         aliceWin = aliceWin||!winnerSquareGame(n-move*move);
+//         }
+//         }
+//         return dp[n] = aliceWin;
+        
+        
+//     }
+// }
