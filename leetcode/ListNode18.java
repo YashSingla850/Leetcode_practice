4940310aadfca5c1559a53eaceb325259e40f151
yashgupta9810 <yashg0621@gmail.com>
2022-01-27 11:18:46 +0530

    12th commit

---
+++ leetcode/ListNode18.java
@@ -0,0 +1,12 @@
+// // LEETCODE 217 DUPLICATE 
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//        Arrays.sort(nums);
+//         for(int i = 0; i<nums.length;i++){
+//             if(i+1<nums.length&& nums[i]==nums[i+1]){
+//                 return true;
+//             }
+//         }
+//        return false;
+//     }
+// }
