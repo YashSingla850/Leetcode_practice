74e29980619a38a2340c26fd975b43c48d49b3aa
yashgupta9810 <yashg0621@gmail.com>
2022-01-27 15:12:30 +0530

    14th commit

---
+++ leetcode/ListNode20.java
@@ -0,0 +1,21 @@
+// Leetcode 169 majority element
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n = nums.length;
+//         int majority = nums[0];
+//         int count =1 ;
+//         for(int i = 1; i<n ; i++){
+//             if(nums[i]== majority){
+//                 count+=1;
+                
+//             }else{
+//                 count-=1;
+//                 if(count==0){
+//                     majority=nums[i];
+//                     count=1;
+//                 }
+//             }
+//         }
+//         return majority;
+//     }
+// }
