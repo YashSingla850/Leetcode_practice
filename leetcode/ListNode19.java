b6dd7105aaacfe4171c865969cd3f1277384e4b5
yashgupta9810 <yashg0621@gmail.com>
2022-01-27 14:11:59 +0530

    13th commit

---
+++ leetcode/ListNode19.java
@@ -0,0 +1,21 @@
+// Leetcode 268 missing number
+
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n  = nums.length;
+//         int expectSum  = (n*(n+1))/2;
+//         int sum = 0;
+        
+    
+//         for(int i = 0 ; i<nums.length;i++){
+//             sum = sum+nums[i];
+            
+                
+//         }
+//         return  expectSum-sum;
+           
+           
+        
+        
+//     }
+// }
