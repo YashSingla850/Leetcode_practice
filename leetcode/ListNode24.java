b669c610f918c28eb78d3979af237efdb96f9e9f
yashgupta9810 <yashg0621@gmail.com>
2022-01-28 12:42:26 +0530

    commit

---
+++ leetcode/ListNode24.java
@@ -0,0 +1,25 @@
+// LEETCODE 283 MOVE ZERO
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         if(nums.length==0||nums.length==1){
+//           return ;
+//         }
+//         int i= 0;
+//         int j = 0;
+//         while(i<nums.length){
+//             if(nums[i]==0){
+//                 i++;
+//             }else{
+//                int temp = nums[j];
+//                 nums[j]=nums[i];
+//                 nums[i]= temp;
+//                 i++;
+//                 j++;
+//             }
+            
+         
+//         }
+//       // return nums;
+        
+//     }
+// }
