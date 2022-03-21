4a4ae796019cc3910f315a5a2fd41b7d17024322
yashgupta9810 <87891888+yashgupta9810@users.noreply.github.com>
2022-03-21 10:47:27 +0530

    Time: 6 ms (83.66%), Space: 45.1 MB (42.72%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
@@ -1,23 +1,44 @@
 class Solution {
     public String kthDistinct(String[] arr, int k) {
-        HashMap<String,Integer>map = new HashMap<>();
+           HashMap<String,Integer>map = new HashMap<>();
         
         for(int i = 0 ; i<arr.length ; i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
             
         }
-     
-      int j=1; 
-        int i= 0;
-        while(i<arr.length){
-            if(map.get(arr[i])==1){
-                if(j==k){
-                    return arr[i];
+        for(String str:arr){
+            if(map.get(str)==1){
+                k--;
+                if(k==0){
+                    return str;
                 }
-                j++;
             }
-            i++;
         }
         return "";
     }
 }
+
+
+// class Solution {
+//     public String kthDistinct(String[] arr, int k) {
+//         HashMap<String,Integer>map = new HashMap<>();
+        
+//         for(int i = 0 ; i<arr.length ; i++){
+//             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            
+//         }
+     
+//       int j=1; 
+//         int i= 0;
+//         while(i<arr.length){
+//             if(map.get(arr[i])==1){
+//                 if(j==k){
+//                     return arr[i];
+//                 }
+//                 j++;
+//             }
+//             i++;
+//         }
+//         return "";
+//     }
+// }
