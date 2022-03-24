0ed807f3290d3b5462267a1b93bf7af78db14aa3
yashgupta9810 <87891888+yashgupta9810@users.noreply.github.com>
2022-03-24 12:14:16 +0530

    Time: 19 ms (27.38%), Space: 42.8 MB (61.13%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,113 @@
+ class Solution {
+     public boolean isIsomorphic(String s, String t) {
+       HashMap<Character,Character>map = new HashMap<>();
+        Set<Character>set = new HashSet<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
+         for(int i = 0 ; i<s.length();i++){
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+            if(map.containsKey(cs)==true){
+                char ch = map.get(cs);
+                if(ch==ct){
+                    continue;
+                }else{
+                    return false;
+                }
+                
+            }else{
+                if(set.contains(ct)){
+                   return false;
+                    
+                }else{
+                    map.put(cs ,ct);
+                    set.add(ct);
+                }
+                
+               
+            } 
+
+            
+            
+         }
+
+
+        return true;
+        
+    }
+}
+
+
+// 2nd atttempt
+
+
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         HashMap<Character,Character>map = new HashMap<>();
+//         Set<Character>set = new HashSet<>();
+//         if(s.length()!=t.length()){
+//             return false;
+//         }else if(s.length()<=1){
+//             return true;
+
+        
+//         }
+//         for(int i = 0 ; i<s.length();i++){
+//             char cs = s.charAt(i);
+//             char ct = t.charAt(i);
+//             if(map.containsKey(cs)==true){
+//                 char doubtvalue = map.get(cs);
+//                 if(doubtvalue==ct){
+//                     if(set.contains(doubtvalue)==true){
+//                         for(char key:map.keySet()){
+//                             char ddvalue = map.get(key);
+//                             if(ddvalue==doubtvalue){
+//                                 if(key==cs){
+//                                     return true;
+//                                 }
+//                             }
+                        
+//                         }
+//                     }
+                    
+//                 }
+                     
+//             }else{
+//                 map.put(cs,ct);
+//                 set.add(ct);
+//              // return true;
+                
+//             }
+//             // return true;
+//         }
+//         return false;
+        
+//     }
+// }
+
+// 1st attmept
+
+
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         HashMap<Character,Character>Map = new HashMap<>()
+// ;
+//         for(int i = 0 ;i<s.length();i++){
+//             char ch = s.charAt(i);
+//             char ch1 = t.charAt(i);
+//             if(map.ContainsKey(ch)==true){
+//                char ch10 =  map.get(ch);
+//                 if(ch10!=ch1){
+//                     return false;
+//                 }else{
+
+//                 map.put(ch,ch1);
+//                 }
+                
+//             }else{
+//                 map.put(ch,ch1);
+//             }
+//         }
+//     }
+// }
