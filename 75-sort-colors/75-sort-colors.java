1bf9cb59ad481fc6da604607b97548b0403d1398
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-05-03 11:57:33 +0530

    Time: 0 ms (100.00%), Space: 42 MB (69.00%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,29 +1,59 @@
+// dutch flag algorithm
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0 , count1 = 0, count2 =0;
-        // int []ans = new int [nums.length];
-        for(int i = 0; i<nums.length;i++){
-            if(nums[i]==0){
-                count0++;
+        int lo = 0;
+        int hi = nums.length-1;
+        int mid = 0;
+        int temp;
+        while(mid<=hi){
+            if(nums[mid]==0){
+
+                nums[mid] = nums[lo];
+                nums[lo] = 0;
+                lo++;
+                mid++;
+            
+            }else if(nums[mid]==2){
+                nums[mid] =nums[hi];
+                nums[hi]=2;
+                hi--;
                 
-            }else if(nums[i]==1){
-                count1++;
             }else{
-                count2++;
+                mid++;
             }
         }
-        int ycount = count0+count1;
-        int kcount = count0+count1+count2;
         
-        for(int i = 0;i<count0;i++){
-            nums[i]=0;
-            
-        }
-        for(int j=  count0 ; j<ycount;j++ ){
-            nums[j]=1;
-        }
-        for(int k =ycount;k<kcount;k++){
-            nums[k]=2;
-        }
     }
 }
+
+//  sorting 1 approach
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int count0 = 0 , count1 = 0, count2 =0;
+//         // int []ans = new int [nums.length];
+//         for(int i = 0; i<nums.length;i++){
+//             if(nums[i]==0){
+//                 count0++;
+                
+//             }else if(nums[i]==1){
+//                 count1++;
+//             }else{
+//                 count2++;
+//             }
+//         }
+//         int ycount = count0+count1;
+//         int kcount = count0+count1+count2;
+        
+//         for(int i = 0;i<count0;i++){
+//             nums[i]=0;
+            
+//         }
+//         for(int j=  count0 ; j<ycount;j++ ){
+//             nums[j]=1;
+//         }
+//         for(int k =ycount;k<kcount;k++){
+//             nums[k]=2;
+//         }
+//     }
+// }
