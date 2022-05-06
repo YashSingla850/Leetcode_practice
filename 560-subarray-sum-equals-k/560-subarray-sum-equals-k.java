5cc634fe98e3d533328f245d65d62b7afef1f632
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-05-06 08:29:54 +0530

    Time: 38 ms (45.85%), Space: 62.7 MB (10.70%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -11,18 +11,12 @@
             ans+=map.get(sum-k);
         }
         if(map.containsKey(sum)){
-            map.put(sum , map.get(sum)+1);
-            
+            map.put(sum , map.get(sum)+1);    
         }else{
             map.put(sum , 1);
-
-        }
-            
-        }
-      
-        return ans;
-        
- 		
+        }     
+      }
+        return ans;	
  	}
  }
 
