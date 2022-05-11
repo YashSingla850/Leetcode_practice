9fc26fc0f58887a37c3fc87992f702c34f6d6f0b
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-05-11 11:31:55 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (78.84%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,34 +1,92 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int pointer1 = m-1,pointer2 = n-1 , pointer3= nums1.length-1;
-        while(pointer3>=0){
+public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int element1 , element2 ; 
-            if(pointer1 >=0){
+        int last = m + n - 1;
 
-            element1 = nums1[pointer1];
-            }else{
+        while (n > 0 && m > 0) {
 
-            element1 = Integer.MIN_VALUE;
+            if (nums2[n - 1] >= nums1[m - 1]) {
+                nums1[last] = nums2[n - 1];
+                n--;
+            } else {
+                nums1[last] = nums1[m - 1];
+                m--;
             }
-            if(pointer2>=0){
+            last--;
+        }
 
-            element2 = nums2[pointer2];
-            }else{
-
-            element2 = Integer.MIN_VALUE;
-            }
-            if(element1>element2){
-                nums1[pointer3] =element1;
-                pointer3--;
-                pointer1--;
-            }else{
-                nums1[pointer3]= element2;
-                pointer3--;
-                pointer2--;
-            }
+        while (n > 0) {
+            nums1[last] = nums2[n - 1];
+            n--;
+            last--;
         }
         
-        
     }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int pointer1 = m-1,pointer2 = n-1 , pointer3= nums1.length-1;
+//         while(pointer3>=0){
+
+//         int element1 , element2 ; 
+//             if(pointer1 >=0){
+
+//             element1 = nums1[pointer1];
+//             }else{
+
+//             element1 = Integer.MIN_VALUE;
+//             }
+//             if(pointer2>=0){
+
+//             element2 = nums2[pointer2];
+//             }else{
+
+//             element2 = Integer.MIN_VALUE;
+//             }
+//             if(element1>element2){
+//                 nums1[pointer3] =element1;
+//                 pointer3--;
+//                 pointer1--;
+//             }else{
+//                 nums1[pointer3]= element2;
+//                 pointer3--;
+//                 pointer2--;
+//             }
+//         }
+        
+        
+//     }
+// }
