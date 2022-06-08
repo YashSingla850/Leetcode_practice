54625a362d960305ef2778293d7a0e31c16be8b1
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-06-08 21:45:29 +0530

    Time: 2 ms (99.08%), Space: 43 MB (96.91%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -0,0 +1,63 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // double ans = 0.0;
+        int n = nums1.length+ nums2.length;
+        int [] temp = new int[n];
+        int i =0;
+        int j =0;
+        int k =0;
+           while(i<nums1.length&&j<nums2.length){
+                    if(nums1[i]<=nums2[j]){
+                        temp[k] = nums1[i];
+                        i++;
+                    }else{
+                        temp[k] = nums2[j];
+                        j++;
+                    }
+               k++;
+           }
+        while(i<nums1.length){
+            temp[k] =nums1[i];
+            k++;
+            i++;
+        }
+          while(j<nums2.length){
+            temp[k] =nums2[j];
+            k++;
+            j++;
+        }
+     ///////////////////////////////
+     if((n)%2!=0)return (double)temp[(n)/2];
+      return (double)(temp[(n)/2]+temp[(n)/2-1])/2;
+        
+        
+        
+    }
+}
+
+
+//         double ans = 0.0;
+//         int n = nums1.length+ nums2.length;
+//         int [] temp = new int[n];
+//         int i =0;
+//         int j =0;
+//         int k =0;
+//         while(k<n){
+//             if(i<nums1.length-1&&j<nums2.length-1){
+//                 if(nums1[i]>nums2[j]){
+//                       temp[k] = nums2[j];
+//                     j++;
+                    
+//                 }else if(nums1[i]<=nums2[j]){
+//                     temp[k] = nums1[i];
+//                     i++;
+//                 }
+//             }else if(i>nums1.length-1&&j<nums2.length-1){
+//                 temp[k] = nums2[j];
+//                 j++;
+//             }else if(j>nums2.length-1&&i<nums1.length-1){
+//                 temp[k] = nums1[i];
+//                 i++;
+//             }
+//         }
+//       k++;
