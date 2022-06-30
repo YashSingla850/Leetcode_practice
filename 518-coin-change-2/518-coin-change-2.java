1867d6824e5ca271e8781e7dd711bcd48d88b84d
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-06-30 18:18:30 +0530

    Time: 8 ms (54.01%), Space: 41.6 MB (77.60%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,3 +1,31 @@
+// TIME LIMIT EXCEED RECURSIVE CODE
+// class Solution {
+//     public int change(int amount, int[] coins) {
+//         return ch(amount , coins ,0);
+        
+//     }
+//     public static int ch(int target, int[]coins , int idx){
+//         if(target==0){
+          
+//            return 1;
+//         }
+//         int count =0;
+//         for(int i = idx;i<coins.length;i++){
+//             if(target-coins[i]>=0){
+//                 count+=ch(target-coins[i],coins,i);
+                
+//             }
+            
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
+
+
 class Solution {
     public int change(int amount, int[] coins) {
         int [] dp = new int[amount+1];
