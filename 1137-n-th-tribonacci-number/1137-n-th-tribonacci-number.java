07613415761fc9003f197fb4936dd399a23a7a27
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-02 10:23:35 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (56.91%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,24 +1,44 @@
 class Solution {
-    public int triboncacci_tabu(int N , int dp[]) {
-        for(int n =0;n<=N;n++){
-             if(n<=2){
-             dp[n] = (n==0?0:1);
-                 continue;
-       }
-         dp[n] =dp[n-1]+dp[n-2]+dp[n-3];   
- }
-      
-        
-        return dp[N];
-    }
     
     public int tribonacci(int n) {
-        int []dp = new int[n+1];
-        int ans = triboncacci_tabu(n,dp);
+        int [] dp = new int[n+1];
+        int ans  = tribonaci_memo( n, dp);
         return ans;
-        
+    }
+    public static int tribonaci_memo(int N , int[]dp){
+        for(int n =0;n<=N;n++){
+           if(n<=2){
+               dp[n] =  (n==0?0:1);
+               continue;
+           }
+            dp[n]=  dp[n-1]+dp[n-2]+dp[n-3];
+        }
+        return dp[N];
     }
 }
+
+
+// class Solution {
+//     public int triboncacci_tabu(int N , int dp[]) {
+//         for(int n =0;n<=N;n++){
+//              if(n<=2){
+//              dp[n] = (n==0?0:1);
+//                  continue;
+//        }
+//          dp[n] =dp[n-1]+dp[n-2]+dp[n-3];   
+//  }
+      
+        
+//         return dp[N];
+//     }
+    
+//     public int tribonacci(int n) {
+//         int []dp = new int[n+1];
+//         int ans = triboncacci_tabu(n,dp);
+//         return ans;
+        
+//     }
+// }
 // memoization
 // class Solution {
 //     public int triboncacci_memo(int n , int dp[]){
