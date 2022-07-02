ea0e1378519b9cd04c090596dd411ba9b7eec80f
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-02 10:52:52 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (11.61%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,35 @@
 class Solution {
     public int climbStairs(int n) {
         int []dp = new int [n+1];
-        return  climbStairs_memo(n ,dp);
+        return  climbStairs_tabu(n ,dp);
     }
-    public int climbStairs_memo(int n , int[]dp){
+    public int climbStairs_tabu(int N , int[]dp){
+        for(int n=0 ;n<=N;n++){
        if(n<=1){
-           return dp[n] =1;
+            dp[n] =1;
+           continue;
        }
-        if(dp[n]!=0){
-            return dp[n];
+      
+         dp[n] = dp[n-1]+dp[n-2];
         }
-        return dp[n] = climbStairs_memo(n-1,dp)+climbStairs_memo(n-2,dp);
+        return dp[N];
     }
+    
 }
+
+
+// class Solution {
+//     public int climbStairs(int n) {
+//         int []dp = new int [n+1];
+//         return  climbStairs_memo(n ,dp);
+//     }
+//     public int climbStairs_memo(int n , int[]dp){
+//        if(n<=1){
+//            return dp[n] =1;
+//        }
+//         if(dp[n]!=0){
+//             return dp[n];
+//         }
+//         return dp[n] = climbStairs_memo(n-1,dp)+climbStairs_memo(n-2,dp);
+//     }
+// }
