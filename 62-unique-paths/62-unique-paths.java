55bad5ca2511a1468d0fee8d6f2708cf890daef4
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-03 10:58:01 +0530

    Time: 1 ms (50.91%), Space: 40.7 MB (69.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -7,21 +7,58 @@ class Solution {
         return ans;
         
     }
-    public int helper(int er , int ec,int[][]dp, int[][]dir){
-      if(er==0||ec==0){
-          return 1;
-      }
-        if(dp[er][ec]!=0){
-            return dp[er][ec];
+    public int helper(int Er , int Ec,int[][]dp, int[][]dir){
+        for(int er = 0;er<=Er;er++){
+            for(int ec =0;ec<=Ec;ec++){
+                  if(er==0||ec==0){
+                 dp[er][ec]= 1;
+                      continue;
+                      
         }
-        int count =0;
-        for(int d =0;d<dir.length;d++){
-            int r = er+dir[d][0];
-            int c = ec+dir[d][1];
-             if(r>=0 && c>=0&&r<dp.length&&c<dp[0].length){
-                 count +=helper(r , c, dp,dir);
-             }
+        
+            int count =0;
+            for(int d =0;d<dir.length;d++){
+                int r = er+dir[d][0];
+                int c = ec+dir[d][1];
+                 if(r>=0 && c>=0&&r<dp.length&&c<dp[0].length){
+                     count +=dp[r][c];
+                 }
+                }
+                 dp[er][ec] = count;
+                
             }
-        return dp[er][ec] = count;
+        }
+        return dp[Er][Ec];
+    
     }
 }
+
+
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int dp [][] =new int [m][n];
+//         int dir[][] =  {{-1,0},{0,-1}};
+//         int er  = m-1 , ec = n-1;
+//         int ans = helper(er , ec, dp ,dir);
+//         return ans;
+        
+//     }
+//     public int helper(int er , int ec,int[][]dp, int[][]dir){
+//       if(er==0||ec==0){
+//           return 1;
+//       }
+//         if(dp[er][ec]!=0){
+//             return dp[er][ec];
+//         }
+//         int count =0;
+//         for(int d =0;d<dir.length;d++){
+//             int r = er+dir[d][0];
+//             int c = ec+dir[d][1];
+//              if(r>=0 && c>=0&&r<dp.length&&c<dp[0].length){
+//                  count +=helper(r , c, dp,dir);
+//              }
+//             }
+//         return dp[er][ec] = count;
+//     }
+// }
