32c3844203c65e1d8f5c6f069bb8dde343eb4585
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-03 11:57:28 +0530

    Time: 2 ms (13.34%), Space: 42 MB (63.83%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+       int n = grid.length , m = grid[0].length , Er = n-1 , Ec = m-1;
+        if(grid[0][0]==1  || grid[Er][Ec]==1){
+            return 0;
+        }
+        int dp[][] = new int[n][m];
+        int dir[][] = {{-1,0},{0,-1}};
+        for(int er =0;er<=Er;er++){
+            for(int ec =0;ec<=Ec;ec++){
+                if(er==0&&ec==0){
+                   dp[er][ec]=1;
+                    continue;
+             }
+                int count =0;
+                for(int d =0;d<dir.length;d++){
+                    int r = er+dir[d][0];
+                    int c = ec+dir[d][1];
+                    if(r>=0 && c>=0 && r<n && c<m &&grid[r][c]==0){
+                       count+=dp[r][c];
+                    }
+                }
+                dp[er][ec] = count;
+                
+            }
+        }
+        return dp[Er][Ec];
+        
+    }
+}
