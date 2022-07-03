fe6399e8924ee5cfb495e2f7a1944d0447d8ecd9
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-03 20:17:39 +0530

    Time: 2844 ms (5.04%), Space: 80.8 MB (5.16%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -8,40 +8,48 @@ class Solution {
         return ans;
         
     }
-      public int longestCommonSubsequence_memo(String s1 ,String s2, int N , int M, int [][]dp){
-          for(int n =0;n<=N;n++){
-            for(int m= 0;m<=M;m++){
-              if(n==0||m==0){
-                dp[n][m] = 0;
-              continue;
-              }
+//       public int longestCommonSubsequence_memo(String s1 ,String s2, int N , int M, int [][]dp){
+//           for(int n =0;n<=N;n++){
+//             for(int m= 0;m<=M;m++){
+//               if(n==0||m==0){
+//                 dp[n][m] = 0;
+//               continue;
+//               }
 
-            if(s1.charAt(n-1)==s2.charAt(m-1)){
-                 dp[n][m] = dp[n-1][m-1]+1;
-            }else{
-                 dp[n][m] = Math.max(dp[n][m-1] ,dp[n-1][m]);
-            }
+//             if(s1.charAt(n-1)==s2.charAt(m-1)){
+//                  dp[n][m] = dp[n-1][m-1]+1;
+//             }else{
+//                  dp[n][m] = Math.max(dp[n][m-1] ,dp[n-1][m]);
+//             }
 
-            }
+//             }
+//           }
+//             return  dp[N][M];
+      
+    // }
+    
+    
+    
+    
+    public int longestCommonSubsequence_memo(String s1 ,String s2, int n , int m, int [][]dp){
+          if(n==0||m==0){
+               return dp[n][m] = 0;
           }
-            return  dp[N][M];
-        
+        if(dp[n][m]!= -1){
+            return dp[n][m];
+        }
+        if(s1.charAt(n-1)==s2.charAt(m-1)){
+            return dp[n][m] = longestCommonSubsequence_memo(s1,s2 , n-1,m-1,dp)+1;
+        }else{
+             return dp[n][m] = Math.max(longestCommonSubsequence_memo(s1,s2 , n,m-1,dp),longestCommonSubsequence_memo(s1,s2 , n-1,m,dp));
+        }
         
     }
-//     public int longestCommonSubsequence_memo(String s1 ,String s2, int n , int m, int [][]dp){
-//           if(n==0||m==0){
-//                return dp[n][m] = 0;
-//           }
-//         if(dp[n][m]!= -1){
-//             return dp[n][m];
-//         }
-//         if(s1.charAt(n-1)==s2.charAt(m-1)){
-//             return dp[n][m] = longestCommonSubsequence_memo(s1,s2 , n-1,m-1,dp)+1;
-//         }else{
-//              return dp[n][m] = Math.max(longestCommonSubsequence_memo(s1,s2 , n,m-1,dp),longestCommonSubsequence_memo(s1,s2 , n-1,m,dp));
-//         }
-        
-//     }
+    
+    
+    
+    
+    
     public void display1D(int[]dp){
         for(int ele:dp){
             System.out.print(ele + " ");
