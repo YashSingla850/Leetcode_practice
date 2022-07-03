f749fffe5d4da2bc87493e7d102d56a403debdf0
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-03 14:46:46 +0530

    Time: 6 ms (11.54%), Space: 46.2 MB (28.80%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -8,21 +8,64 @@ class Solution {
        int ans =  helper(er , ec, dp , dir , grid);
         return ans;
     }
-    public int helper(int er , int ec, int[][]dp , int [][]dir , int [][]grid){
-          if(er==0&&ec==0){
-           return  dp[er][ec] = grid[er][ec];
-           }
-        if(dp[er][ec]!=-1){
-            return dp[er][ec];
+    public int helper(int Er , int Ec, int[][]dp , int [][]dir , int [][]grid){
+        for(int er = 0;er<=Er;er++){
+            for(int ec = 0;ec<=Ec;ec++){
+                if(er==0&&ec==0){
+                      dp[er][ec] = grid[er][ec];
+                    continue;
+               }
+                
+            int min = (int)1e9;
+            for(int d = 0;d<dir.length;d++){
+                int r = er+dir[d][0];
+                int c = ec+dir[d][1];
+                if(r>=0 && c>=0 && r<grid.length && c<grid[0].length){
+                    min = Math.min(min, dp[r][c]);
+                 }
+            }
+               dp[er][ec] = min+grid[er][ec];
+
+            }
         }
-        int min = (int)1e9;
-        for(int d = 0;d<dir.length;d++){
-            int r = er+dir[d][0];
-            int c = ec+dir[d][1];
-            if(r>=0 && c>=0 && r<grid.length && c<grid[0].length){
-                min = Math.min(min, helper(r , c, dp ,dir, grid));
-             }
-        }
-        return dp[er][ec] = min+grid[er][ec];
+        return dp[Er][Ec];
+         
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int minPathSum(int[][] grid) {
+//         int n = grid.length, m = grid[0].length;
+//         int er = n-1,ec = m-1;
+//         int dp [][]=  new int [n][m];
+//         for(int []d :dp) Arrays.fill(d,-1);
+//         int dir[][] = {{-1,0},{0,-1}};
+//        int ans =  helper(er , ec, dp , dir , grid);
+//         return ans;
+//     }
+//     public int helper(int er , int ec, int[][]dp , int [][]dir , int [][]grid){
+//           if(er==0&&ec==0){
+//            return  dp[er][ec] = grid[er][ec];
+//            }
+//         if(dp[er][ec]!=-1){
+//             return dp[er][ec];
+//         }
+//         int min = (int)1e9;
+//         for(int d = 0;d<dir.length;d++){
+//             int r = er+dir[d][0];
+//             int c = ec+dir[d][1];
+//             if(r>=0 && c>=0 && r<grid.length && c<grid[0].length){
+//                 min = Math.min(min, helper(r , c, dp ,dir, grid));
+//              }
+//         }
+//         return dp[er][ec] = min+grid[er][ec];
+//     }
+// }
