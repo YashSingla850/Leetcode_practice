0f9e5586bb02aa7e9a2c31e1da23a7af353f359c
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-04 15:52:50 +0530

    Time: 18 ms (37.44%), Space: 46.7 MB (65.42%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -6,23 +6,52 @@ class Solution {
         int ans = helper(s1 , s2, n, m, dp);
         return n+m-2*ans;
     }
-    public int helper(String s1 ,String s2 , int n , int m , int [][]dp){
-        if(n==0||m==0){
-            return dp[n][m] =0;
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        int a = helper(s1, s2, n-1, m-1,dp);
-        int b = helper(s1 , s2, n , m-1 ,dp);
-        int c = helper(s1 ,s2 , n-1, m, dp);
+    public int helper(String s1 ,String s2 , int N , int M , int [][]dp){
+        for(int n =0;n<=N;n++){
+            for(int m =0;m<=M;m++){
+                   if(n==0||m==0){
+                     dp[n][m] =0;
+                       continue;
+            }
         if(s1.charAt(n-1)==s2.charAt(m-1)){
-            return dp[n][m]= a+1;
+              dp[n][m]= dp[n-1][m-1]+1;
         }else{
-            return dp[n][m] = Math.max(b,c);
+             dp[n][m] = Math.max(dp[n][m-1],dp[n-1][m]);
         }
+                
+            }
+        }
+        return dp[N][M];
+     
     }
 }
+
+
+// class Solution {
+//     public int minDistance(String s1, String s2) {
+//         int n = s1.length(), m = s2.length();
+//         int [][]dp = new int [n+1][m+1];
+//         for(int[]d : dp)Arrays.fill(d,-1);
+//         int ans = helper(s1 , s2, n, m, dp);
+//         return n+m-2*ans;
+//     }
+//     public int helper(String s1 ,String s2 , int n , int m , int [][]dp){
+//         if(n==0||m==0){
+//             return dp[n][m] =0;
+//         }
+//         if(dp[n][m]!=-1){
+//             return dp[n][m];
+//         }
+//         int a = helper(s1, s2, n-1, m-1,dp);
+//         int b = helper(s1 , s2, n , m-1 ,dp);
+//         int c = helper(s1 ,s2 , n-1, m, dp);
+//         if(s1.charAt(n-1)==s2.charAt(m-1)){
+//             return dp[n][m]= a+1;
+//         }else{
+//             return dp[n][m] = Math.max(b,c);
+//         }
+//     }
+// }
 
 
 
