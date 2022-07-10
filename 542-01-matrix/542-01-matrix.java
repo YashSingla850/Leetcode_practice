0a18345886735f8f6a821f8251923dcca4c10e80
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-10 12:30:08 +0530

    Time: 50 ms (8.59%), Space: 79.9 MB (8.16%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,47 +1,43 @@
 class Solution {
     class pair{
-        int i ,j ,level;
-        pair(int i ,int j ,int level){
-            this.i =i;
-            this.j =j;
-            this.level =level;
+        int i;
+        int j ;
+        int level;
+        pair(int i , int j , int level){
+            this.i = i;
+            this.j = j;
+            this.level = level;
         }
     }
     public int[][] updateMatrix(int[][] mat) {
+        int [][]ans = new int[mat.length][mat[0].length];
         boolean [][] visited = new boolean[mat.length][mat[0].length];
-        int [][]ans = new int [mat.length][mat[0].length];
-       
-        ArrayDeque<pair> q  = new ArrayDeque<>();
-        for(int i =0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
+        ArrayDeque<pair>queue = new ArrayDeque<>();
+        for(int i = 0;i<mat.length;i++){
+            for(int j =0;j<mat[0].length;j++){
                 if(mat[i][j]==0){
-                    q.add(new pair(i ,j ,0)); 
-                }  
+                    queue.add(new pair(i,j,0));
+                }
             }
         }
-       
-       
-         while(q.size()>0){
-             pair rem = q.remove();
-             if(visited[rem.i][rem.j]==true){
-                 continue;
-             }
-             visited[rem.i][rem.j]=true;
-              ans[rem.i][rem.j] = rem.level;
-             
-             addc(rem.i+1 ,rem.j ,rem.level+1  ,visited,q);
-              addc(rem.i-1 ,rem.j ,rem.level+1 ,visited,q);
-              addc(rem.i ,rem.j-1 ,rem.level+1 ,visited,q);
-              addc(rem.i ,rem.j+1 ,rem.level+1 ,visited,q);
-  
-         }
-       return ans;
+        while(queue.size()>0){
+            pair rem= queue.remove();
+            if(visited[rem.i][rem.j]==true){
+                continue;
+            }
+            visited[rem.i][rem.j] = true;
+            ans[rem.i][rem.j] = rem.level;
+            addn(rem.i-1,rem.j , rem.level+1,visited ,queue);
+            addn(rem.i,rem.j-1 , rem.level+1,visited ,queue);
+            addn(rem.i,rem.j+1 , rem.level+1,visited ,queue);
+            addn(rem.i+1,rem.j , rem.level+1,visited ,queue);
+        }
+        return ans;
     }
-          void addc( int i ,int j , int level  ,boolean[][]visited, ArrayDeque<pair>q){
-         
-              if(i<0 || j<0 || i>=visited.length || j>=visited[0].length || visited[i][j]==true){
-                     return;
-              }
-              q.add(new pair(i ,j ,level));
-          }
+    void addn(int i , int j , int level , boolean[][]visited ,ArrayDeque<pair>queue){
+        if(i<0||j<0||i>=visited.length||j>=visited[0].length|| visited[i][j]==true){
+            return;
+        }
+        queue.add(new pair(i, j ,level));
+    }
 }
