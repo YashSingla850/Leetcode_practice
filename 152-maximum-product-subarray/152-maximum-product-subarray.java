5f52ef2d5a712c872c0da277bb112a5eae642fda
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-12 10:16:23 +0530

    Time: 3 ms (40.19%), Space: 44.9 MB (41.75%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,19 +1,38 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max = -(int)1e9;
-       
-        for(int i =0;i<nums.length;i++){
-            int ans =nums[i];
-            if(max<ans){
-                max = ans;
-            }
-            for(int j =i+1;j<nums.length;j++){
-                ans = ans*nums[j];
-                if(max<ans){
-                    max = ans;
-                }
-            }
+      int result = nums[0];
+        int max = nums[0];
+        int min = nums[0];
+        for(int i =1;i<nums.length;i++){
+            int curMax = max;
+         max =  Math.max(nums[i],Math.max(max*nums[i],min*nums[i]));
+            min = Math.min(nums[i],Math.min(curMax*nums[i],min*nums[i]));
+            result = Math.max(result , max);
         }
-        return max;
+        return result;
     }
 }
+
+
+
+
+// BRUTE FORCE -:
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         int max = -(int)1e9;
+       
+//         for(int i =0;i<nums.length;i++){
+//             int ans =nums[i];
+//             if(max<ans){
+//                 max = ans;
+//             }
+//             for(int j =i+1;j<nums.length;j++){
+//                 ans = ans*nums[j];
+//                 if(max<ans){
+//                     max = ans;
+//                 }
+//             }
+//         }
+//         return max;
+//     }
+// }
