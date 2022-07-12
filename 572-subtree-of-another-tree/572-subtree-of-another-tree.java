57d7baf1bd78e0dda4e6a1926dea9d6eb18875d7
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-12 11:03:18 +0530

    Time: 3 ms (97.09%), Space: 46.8 MB (34.37%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -13,56 +13,52 @@
  *     }
  * }
  */
-
 class Solution {
-public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-if(subRoot==null) return true;
-if(root==null) return false;
-if(root.val==subRoot.val){
-if(isIdentical(root,subRoot)){
-return true;
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(subRoot==null) return true;
+        if(root==null) return false;
+          
+        if(root.val==subRoot.val){
+            if( helper(root , subRoot)){
+                return true;
+            }
+        }
+        return (isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot));
+          
+    }
+    public boolean helper(TreeNode subroot , TreeNode root){
+        if(root ==null&&subroot==null){
+            return true;
+        }
+        if(subroot==null || root==null){
+            return false;
+        }
+        if(subroot.val==root.val){
+        return (helper(subroot.left,root.left) && helper(subroot.right , root.right));
+        }
+        return false;
+    }
 }
-}
-return (isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot));
-}
-
-public boolean isIdentical(TreeNode root, TreeNode subRoot){
-if(root==null && subRoot==null) return true;
-if(root==null || subRoot==null) return false;
-if(root.val==subRoot.val){
-return (isIdentical(root.left,subRoot.left) && isIdentical(root.right,subRoot.right));
-}
-return false;
-}
-}
-
-
 
 
 // class Solution {
-//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-//        if(root==null){
-//            return false;
-//        }
-//         if(subRoot==null) return true;
-//         if(root.val==subRoot.val){
-//              return helper(root,subRoot);
-            
-//         }
-//         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-//     }
-//     public boolean helper(TreeNode n1 , TreeNode n2){
-//         if(n1==null&&n2==null){
-//             return true;
-//         } 
-//         if(n1==null&&n2!=null||n1!=null&&n2==null){
-//             return false;
-//         }
-//        if(n1.val ==n2.val){
-        
-//         return (helper(n1.left,n2.left)&&helper(n1.right,n2.right));
-//        }
-//         return false;
-//     }
+// public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+// if(subRoot==null) return true;
+// if(root==null) return false;
+// if(root.val==subRoot.val){
+// if(isIdentical(root,subRoot)){
+// return true;
+// }
+// }
+// return (isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot));
 // }
 
+// public boolean isIdentical(TreeNode root, TreeNode subRoot){
+// if(root==null && subRoot==null) return true;
+// if(root==null || subRoot==null) return false;
+// if(root.val==subRoot.val){
+// return (isIdentical(root.left,subRoot.left) && isIdentical(root.right,subRoot.right));
+// }
+// return false;
+// }
+// }
