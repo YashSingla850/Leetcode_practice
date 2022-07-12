2aa7d6455c6524db8ee3fb1f10598de49306b8b6
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-12 20:07:32 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+          ArrayList<Integer>list = new ArrayList<>();
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // ArrayList<Integer>list = new ArrayList<>();
+       boolean[]visited = new boolean[V];
+       for(int v =0;v<V;v++){
+           if(visited[v]==false){
+               dfs(v,adj,visited);
+           }
+       }
+       return list;
+    }
+    public  void dfs(int v,ArrayList<ArrayList<Integer>> adj,boolean[]visited){
+        list.add(v);
+        visited[v]=true;
+        for(int nbr:adj.get(v)){
+            if(visited[nbr]==false){
+                dfs(nbr , adj,visited);
+            }
+        }
+    }
+}
