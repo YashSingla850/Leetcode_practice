b7cbe802af46c5338826e7a2e7f6b8b9defe9a8f
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-13 15:42:06 +0530

    Time: 47 ms (54.78%), Space: 42.7 MB (61.23%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,24 +1,19 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int [][][] dp = new int[m+1][n+1][strs.length];
-        return helper(strs, m , n,0,dp);
+        int [][] dp = new int[m+1][n+1];
+      for(String s :strs){
+          int [] count = count(s);
+          for(int zero = m ; zero>=count[0];zero--){
+              for(int one = n; one>=count[1];one--){
+                  dp[zero][one] = Math.max(dp[zero-count[0]][one-count[1]]+1, dp[zero][one]);
+              }
+          }
+          
+      }
+       return dp[m][n];
     }
-    public int helper(String []strs , int m0 , int n1,int index,int dp[][][]){
-        if(index==strs.length|| m0+n1==0){
-            return 0;
-        }
-        if(dp[m0][n1][index]!=0){
-            return dp[m0][n1][index];
-        }
-        int[]arr = count(strs[index]);
-        int cans =0;
-        if(m0>=arr[0]&&n1>=arr[1]){
-              cans = 1+helper(strs , m0-arr[0] , n1-arr[1], index+1,dp);
-        }
-        int skip=  helper(strs , m0 ,n1, index+1,dp);
-        dp[m0][n1][index] =  Math.max(cans , skip);
-        return dp[m0][n1][index];
-    }
+    
+    
     int [] count(String s){
         int []count = new int[2];
         for(char c: s.toCharArray()){
@@ -27,3 +22,35 @@ class Solution {
         return count;
     }
 }
+// class Solution {
+//     public int findMaxForm(String[] strs, int m, int n) {
+//         int [][][] dp = new int[m+1][n+1][strs.length];
+//         return helper(strs, m , n,0,dp);
+//     }
+//     public int helper(String []strs , int m0 , int n1,int index,int dp[][][]){
+//         for(int idx =0;idx<=Index;idx++){
+//             for(int m0 =0 , n1 =0;)
+//         }
+//         if(index==strs.length|| m0+n1==0){
+//             return 0;
+//         }
+//         if(dp[m0][n1][index]!=0){
+//             return dp[m0][n1][index];
+//         }
+//         int[]arr = count(strs[index]);
+//         int cans =0;
+//         if(m0>=arr[0]&&n1>=arr[1]){
+//               cans = 1+helper(strs , m0-arr[0] , n1-arr[1], index+1,dp);
+//         }
+//         int skip=  helper(strs , m0 ,n1, index+1,dp);
+//         dp[m0][n1][index] =  Math.max(cans , skip);
+//         return dp[m0][n1][index];
+//     }
+//     int [] count(String s){
+//         int []count = new int[2];
+//         for(char c: s.toCharArray()){
+//             count[c-'0']++;
+//         }
+//         return count;
+//     }
+// }
