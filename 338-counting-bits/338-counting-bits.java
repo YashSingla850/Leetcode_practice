ac4304423e09a4edc0e013bb07340e77339f516e
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-14 12:26:49 +0530

    Time: 1 ms (99.98%), Space: 46.5 MB (88.62%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] countBits(int n) {
         int bits[] = new int[n + 1];
-        
-        for (int i = 0; i <= n; i++) {
+        bits[0] =0;
+        for (int i = 1; i <= n; i++) {
 		
 			//bitCount will return number of set bits in given number.
-            bits[i] = Integer.bitCount(i);
+            bits[i] = bits[i/2]+i%2;
         }
         
         return bits;
