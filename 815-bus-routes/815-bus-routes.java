379ab7a7be604ef9de24faa8245737350362ef5b
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-15 19:56:43 +0530

    Time: 77 ms (68.90%), Space: 90.9 MB (58.47%) - LeetHub

--- 815-bus-routes/815-bus-routes.java
+++ 815-bus-routes/815-bus-routes.java
@@ -1,51 +1,100 @@
 class Solution {
-     class Pair{
+     class pair{
          int bus;
             int csf;
-            Pair(int bus ,int csf){
+            pair(int bus ,int csf){
                 this.bus = bus;
                 this.csf = csf;
             }
         }
     public int numBusesToDestination(int[][] routes, int source, int target) {
-       if(source == target)
-           return 0;
-        HashMap<Integer ,ArrayList<Integer>> map = new HashMap<>();
-        for(int bus =0 ;bus<routes.length;bus++){
-            for(int stop : routes[bus]){
+            if(source==target)return 0;
+        HashMap<Integer,ArrayList<Integer>>map  = new HashMap<>();
+        for(int bus =0;bus<routes.length;bus++){
+            for(int stop:routes[bus]){
                 if(map.containsKey(stop)==false){
-                     map.put(stop , new ArrayList<>());
+                    map.put(stop,new ArrayList<>());
                 }
                 map.get(stop).add(bus);
             }
         }
-        ArrayDeque<Pair> q = new ArrayDeque<>();
-        for(int bsrc:map.get(source)){
-             q.add(new Pair(bsrc,1));
+        ArrayDeque<pair> queue = new ArrayDeque<>();
+        for(int bsrc: map.get(source)){
+            queue.add(new pair(bsrc , 0));
         }
-        boolean[] visited = new boolean[routes.length];
-        while(q.size()>0){
-            Pair rem = q.remove();
+        boolean[]visited = new boolean[routes.length];
+        while(queue.size()>0){
+            pair rem = queue.remove();
             if(visited[rem.bus]==true){
                 continue;
             }
-            visited[rem.bus] =true;
-             for(int rstop:routes[rem.bus]){
-                 if(rstop==target){
-                     return rem.csf;
-                 }
-             }
+            visited[rem.bus]=true;
+            
             for(int rstop:routes[rem.bus]){
+                if(rstop==target){
+                    return rem.csf+1;
+                }
                 for(int bus:map.get(rstop)){
                     if(visited[bus]==false){
-                        q.add(new Pair(bus,rem.csf+1));
+                        queue.add(new pair(bus,rem.csf+1));
                     }
                 }
             }
         }
         return -1;
-        
-        
-        
     }
 }
+
+
+
+// class Solution {
+//      class Pair{
+//          int bus;
+//             int csf;
+//             Pair(int bus ,int csf){
+//                 this.bus = bus;
+//                 this.csf = csf;
+//             }
+//         }
+//     public int numBusesToDestination(int[][] routes, int source, int target) {
+//        if(source == target)
+//            return 0;
+//         HashMap<Integer ,ArrayList<Integer>> map = new HashMap<>();
+//         for(int bus =0 ;bus<routes.length;bus++){
+//             for(int stop : routes[bus]){
+//                 if(map.containsKey(stop)==false){
+//                      map.put(stop , new ArrayList<>());
+//                 }
+//                 map.get(stop).add(bus);
+//             }
+//         }
+//         ArrayDeque<Pair> q = new ArrayDeque<>();
+//         for(int bsrc:map.get(source)){
+//              q.add(new Pair(bsrc,1));
+//         }
+//         boolean[] visited = new boolean[routes.length];
+//         while(q.size()>0){
+//             Pair rem = q.remove();
+//             if(visited[rem.bus]==true){
+//                 continue;
+//             }
+//             visited[rem.bus] =true;
+//              for(int rstop:routes[rem.bus]){
+//                  if(rstop==target){
+//                      return rem.csf;
+//                  }
+//              }
+//             for(int rstop:routes[rem.bus]){
+//                 for(int bus:map.get(rstop)){
+//                     if(visited[bus]==false){
+//                         q.add(new Pair(bus,rem.csf+1));
+//                     }
+//                 }
+//             }
+//         }
+//         return -1;
+        
+        
+        
+//     }
+// }
