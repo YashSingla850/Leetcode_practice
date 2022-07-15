0b6b1b0b722a57f8474a50a584880eb0ed2ec3d6
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-15 17:55:04 +0530

    Time: 6 ms (17.21%), Space: 54.5 MB (10.15%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,45 +1,92 @@
 class Solution {
-    class pair{
-        int v;
-        int color;
-        pair(int v, int color){
-            this.v = v;
-            this.color=color;
-        }
-    }
+   class pair{
+       int v ;
+       int color;
+       pair(int v,int color){
+           this.v = v;
+           this.color = color;
+       }
+   }
     
     public boolean isBipartite(int[][] graph) {
-        
         int []visited = new int[graph.length];
-        for(int v =0;v<graph.length;v++){
-            if(visited[v]==0){
-                boolean ans = helper(graph , visited , v);
-                if(ans==false){
-                    return false;
-                }
-            }
-        }
+         // boolean flag = true;
+           for(int v =0;v<graph.length;v++){
+               if(visited[v]==0){
+                  boolean flag =  bfs(graph , v, visited);
+                   if(flag==false){
+                       return false;
+                   }
+               }
+               
+           }
         return true;
     }
+          public boolean bfs(int[][]graph , int v, int[]visited){
+              ArrayDeque<pair>queue = new ArrayDeque<>();
+              queue.add(new pair(v,1));
+              while(queue.size()>0){
+                  pair rem = queue.remove();
+                  if(visited[rem.v]!=0){
+                      int oc = visited[rem.v];
+                      int nc = rem.color;
+                      if(oc==nc)continue;
+                        return false;
+                  }
+                  visited[rem.v] = rem.color;
+                  for(int nbr:graph[rem.v]){
+                      if(visited[nbr]==0){
+                          queue.add(new pair(nbr , rem.color*-1));
+                      }
+                  }
+              }
+              return true;
+          }
+    }
+
+
+// class Solution {
+//     class pair{
+//         int v;
+//         int color;
+//         pair(int v, int color){
+//             this.v = v;
+//             this.color=color;
+//         }
+//     }
     
-    public boolean helper(int[][]graph ,int[]visited , int v){
-        ArrayDeque<pair>queue = new ArrayDeque<>();
-        queue.add(new pair(v,1));
-        while(queue.size()>0){
-            pair rem = queue.remove();
-            if(visited[rem.v]!=0){
-                int oc = visited[rem.v];
-                int nc  = rem.color;
-                if(oc==nc)continue;
-                return false;
-            }
-            visited[rem.v] = rem.color;
-            for(int nbr:graph[rem.v]){
-                if(visited[nbr]==0){
-                    queue.add(new pair(nbr , rem.color*-1));
-                }
-            }
-        }
-        return true;
-    }
-}
+//     public boolean isBipartite(int[][] graph) {
+        
+//         int []visited = new int[graph.length];
+//         for(int v =0;v<graph.length;v++){
+//             if(visited[v]==0){
+//                 boolean ans = helper(graph , visited , v);
+//                 if(ans==false){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+    
+//     public boolean helper(int[][]graph ,int[]visited , int v){
+//         ArrayDeque<pair>queue = new ArrayDeque<>();
+//         queue.add(new pair(v,1));
+//         while(queue.size()>0){
+//             pair rem = queue.remove();
+//             if(visited[rem.v]!=0){
+//                 int oc = visited[rem.v];
+//                 int nc  = rem.color;
+//                 if(oc==nc)continue;
+//                 return false;
+//             }
+//             visited[rem.v] = rem.color;
+//             for(int nbr:graph[rem.v]){
+//                 if(visited[nbr]==0){
+//                     queue.add(new pair(nbr , rem.color*-1));
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
