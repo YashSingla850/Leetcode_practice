8070538a29385819b80448727195bd9b18611558
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-16 10:59:39 +0530

    Time: 5 ms (55.59%), Space: 72.5 MB (73.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -6,12 +6,13 @@ class Solution {
         int i =0;
           while(i<j){
               int breath = j-i;
-            int temp = Math.min(height[i],height[j])*breath;
-              if(max<temp){
-                  max = temp;
+              int length1 = Math.min(height[i],height[j]);
+            int area = length1*breath;
+              if(max<area){
+                  max = area;
               }
               if(height[i]!=height[j]){
-             int tmp1 = Math.min(height[i],height[j]);
+             int tmp1 = length1;
               if(tmp1==height[i]){
                   i++;
                }else{
