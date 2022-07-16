5a348ff178bf0d20c01f9855b0e6fa83760e06f0
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-16 17:45:04 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (57.85%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -13,32 +13,75 @@
  *     }
  * }
  */
+
+
 class Solution {
+     List<Integer> ans;
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer>ans = new ArrayList<>();
-        TreeNode cur = root;
-        while(cur!=null){
-            if(cur.left==null){
-                ans.add(cur.val);
-                cur =cur.right;
-                
-            }else{
-                TreeNode iop = cur.left;
-                while(iop.right!=null&&iop.right!=cur){
-                    iop = iop.right;
-                }
-                    if(iop.right==null){
-                        iop.right=cur;
-                        cur = cur.left;
-                    }else{
-                        iop.right =null;
-                        ans.add(cur.val);   
-                        cur = cur.right;
-                        
-                    }
-            }
+       ans = new ArrayList<>();
+        if(root==null){
+            return new ArrayList<>();
         }
+        helper(root);    
         return ans;
-        
+    }
+    public void helper(TreeNode node){
+        if(node==null){
+            return ;
+        }
+        helper(node.left);
+        if(node.left==null ){
+            // ans.add(null);
+            ans.add(node.val);
+        }else{
+            ans.add(node.val);
+        }
+        helper(node.right);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer>ans = new ArrayList<>();
+//         TreeNode cur = root;
+//         while(cur!=null){
+//             if(cur.left==null){
+//                 ans.add(cur.val);
+//                 cur =cur.right;
+                
+//             }else{
+//                 TreeNode iop = cur.left;
+//                 while(iop.right!=null&&iop.right!=cur){
+//                     iop = iop.right;
+//                 }
+//                     if(iop.right==null){
+//                         iop.right=cur;
+//                         cur = cur.left;
+//                     }else{
+//                         iop.right =null;
+//                         ans.add(cur.val);   
+//                         cur = cur.right;
+                        
+//                     }
+//             }
+//         }
+//         return ans;
+        
+//     }
+// }
