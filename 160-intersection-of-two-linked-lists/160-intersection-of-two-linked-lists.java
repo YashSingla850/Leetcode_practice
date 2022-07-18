57c82e42224998116652a74f18696270d11830b3
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-18 19:16:29 +0530

    Time: 3 ms (30.69%), Space: 56.3 MB (6.69%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -9,27 +9,62 @@
  *     }
  * }
  */
+
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA==null || headB==null){
-            return null;
-        }
-        ListNode pa = headA;
-        ListNode pb = headB;
-        
-        while(pa!=pb){
-            if(pa==null){
-                pa=headB;
+    public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+        if(head1==null || head2==null)return null;
+      
+        ListNode p1 = head1 ,p2 = head2;
+        while(p1!=p2){
+            if(p1==null){
+                p1 = head2;
             }else{
-                pa = pa.next;
+                p1 = p1.next;
             }
-            if(pb==null){
-                pb= headA;
+            if(p2==null){
+                p2 = head1;
             }else{
-                pb = pb.next;
+                p2 = p2.next;
             }
-            
         }
-        return pa;
+        return p1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         if(headA==null || headB==null){
+//             return null;
+//         }
+//         ListNode pa = headA;
+//         ListNode pb = headB;
+        
+//         while(pa!=pb){
+//             if(pa==null){
+//                 pa=headB;
+//             }else{
+//                 pa = pa.next;
+//             }
+//             if(pb==null){
+//                 pb= headA;
+//             }else{
+//                 pb = pb.next;
+//             }
+            
+//         }
+//         return pa;
+//     }
+// }
