bb567da30316694a61532180f165557e2f0e0268
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-25 17:05:01 +0530

    Time: 179 ms (11.04%), Space: 44.6 MB (40.99%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,24 +1,66 @@
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int []dp = new int[nums.length];
+//         int ans = helper(nums, dp);
+//         return ans;
+       
+//     }
+//     public int helper(int []nums , int[]dp){
+//        int n = nums.length, maxLen =0;
+//         for(int i = 0;i<n;i++){
+//             dp[i] = 1;
+//             for(int j =i-1;j>=0;j--){
+//                 if(nums[j]<nums[i]){
+//                     dp[i] = Math.max(dp[i],dp[j]+1);
+//                 }
+//             }
+//             maxLen  = Math.max(maxLen,dp[i]);
+//         }
+//         return maxLen;
+//     }
+// }
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int []dp = new int[nums.length];
-        int ans = helper(nums, dp);
-        return ans;
-       
+        int n = nums.length;
+      int []dp = new int [n];
+        int max = -(int)1e9;
+    for(int i  =0;i<n;i++){
+        int ans = helper(nums  , i , dp);
+        if(ans>max)max = ans;
     }
-    public int helper(int []nums , int[]dp){
-       int n = nums.length, maxLen =0;
-        for(int i = 0;i<n;i++){
-            dp[i] = 1;
-            for(int j =i-1;j>=0;j--){
-                if(nums[j]<nums[i]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
+        return max;
+    }
+    public int helper(int []nums  , int idx , int []dp){
+        if(dp[idx]!=0)return dp[idx];
+        int maxLen = 1;
+        for(int j  = idx-1 ; j>=0;j--){
+            if(nums[j]<nums[idx]){
+                int recans = helper(nums , j, dp);
+                maxLen = Math.max(maxLen , recans+1);
             }
-            maxLen  = Math.max(maxLen,dp[i]);
         }
-        return maxLen;
+        return dp[idx]= maxLen;
     }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -36,7 +78,7 @@ class Solution {
 //         if(dp[idx]!=0){
 //             return dp[idx];
 //         }
-//         int maxLen = 1;
+        // int maxLen = 1;
 //         for(int j = idx-1 ; j>=0;j--){
 //             if(nums[j]<nums[idx]){
 //                 int recans = helper(nums,j , dp);
