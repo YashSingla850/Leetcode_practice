ceeb2653fc2da30a328d081781624240312d55cd
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-28 16:32:25 +0530

    Time: 2 ms (78.21%), Space: 45 MB (41.70%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,17 +1,43 @@
 class Solution {
     public int maxProduct(int[] nums) {
-      int result = nums[0];
-        int max = nums[0];
-        int min = nums[0];
-        for(int i =1;i<nums.length;i++){
-            int curMax = max;
-         max =  Math.max(nums[i],Math.max(max*nums[i],min*nums[i]));
-            min = Math.min(nums[i],Math.min(curMax*nums[i],min*nums[i]));
-            result = Math.max(result , max);
+        int res = Integer.MIN_VALUE;
+        int prd =1;
+        for(int i =0;i<nums.length;i++){
+            prd = prd*nums[i];
+            res = Math.max(res , prd);
+            if(prd==0){
+                prd =1;
+            }
         }
-        return result;
+        prd = 1;
+        for(int i =nums.length-1;i>=0;i--){
+            prd  = prd*nums[i];
+            res =Math.max(res, prd);
+            if(prd==0){
+                prd =1;
+            }
+        }
+        return res;
     }
 }
+
+
+
+// O(n) Approach -:
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//       int result = nums[0];
+//         int max = nums[0];
+//         int min = nums[0];
+//         for(int i =1;i<nums.length;i++){
+//             int curMax = max;
+//          max =  Math.max(nums[i],Math.max(max*nums[i],min*nums[i]));
+//             min = Math.min(nums[i],Math.min(curMax*nums[i],min*nums[i]));
+//             result = Math.max(result , max);
+//         }
+//         return result;
+//     }
+// }
 
 
 
