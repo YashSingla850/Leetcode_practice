80ee0fdcb7aa7d6b6af679dc823394c0721cdd6f
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-07-30 18:45:47 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (99.87%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,51 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n= nums.length;
+        
+//         int dp [] = new int [n+1];
+//         // Arrays.fill(dp,-1);
+//         return helper(nums , n-1 , dp);
+//     }
+//     public int helper(int[]nums , int N, int []dp){
+//         for(int n =0;n<=N;n++){
+//         if(n==0){
+//             dp[n] =   nums[n];
+//             continue;
+//         }
+//         if(n<0){ 
+//             dp[n] = 0;
+//         }
+//         // if(dp[n]!=0)return dp[n];
+//         int pick = dp[n-1];
+//         int dpick = dp[n-2]+nums[n];
+//           dp[n] = Math.max(pick , dpick);
+        
+//         }
+//         return dp[N];
+//     }
+// }
+
+// ek test case nahi pass hua
+
+class Solution {
+    public int rob(int[] nums) {
+        int n= nums.length;
+        
+        int dp [] = new int [n+1];
+        Arrays.fill(dp,-1);
+        return helper(nums , n-1 , dp);
+    }
+    public int helper(int[]nums , int n, int []dp){
+        if(n==0){
+            return  nums[n];
+        }
+        if(n<0){ 
+            return 0;
+        }
+        if(dp[n]!=-1)return dp[n];
+        int pick = helper(nums ,n-1 ,dp );
+        int dpick = helper(nums, n-2 ,dp)+nums[n];
+         
+        return dp[n] = Math.max(pick , dpick);
+    }
+}
