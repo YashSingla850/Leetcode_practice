7552eaba7e1f1bfb02dbf7adf5700298671c1f4d
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-08-01 09:44:12 +0530

    Time: 2 ms (7.67%), Space: 41 MB (48.79%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,28 @@
-class Solution {
-    public int uniquePaths(int m, int n) {
-        int [][] dp = new int[n][m];
-        for(int []d:dp)Arrays.fill(d,-1);
-        return helper(dp , n-1, m-1);
+class Solution{
+    public int uniquePaths(int n , int m){
+        int [][]dp = new int [n][m];
+        int [][]dir = {{-1,0},{0,-1}};
+        return helper(dp , n-1,m-1,dir);
     }
-    public int helper(int [][]dp  , int n , int m){
-        if(n==0 && m==0){
-            return  1;
+    public int helper(int [][]dp , int N, int M,int[][]dir){
+        for(int n=0;n<=N;n++){
+            for(int m =0;m<=M;m++){
+                if(n==0 && m==0){
+                    dp[n][m] =1;
+                    continue;
+                }
+                int count =0;
+                for(int d =0;d<dir.length;d++){
+                    int r = n+dir[d][0];
+                    int c  = m+dir[d][1];
+                    if(r>=0&&c>=0&&r<dp.length&&c<dp[0].length){
+                        count+= dp[r][c];
+                    }
+                }
+                dp[n][m] = count;
+            }
         }
-        int count = 0;
-        if(n<0 || m<0 || n>=dp.length || m>=dp[0].length){
-            return 0;
-        }
-        if(dp[n][m]!=-1)return dp[n][m];
-        
-         dp[n][m]= helper(dp , n-1 , m)+helper(dp , n ,m-1);
-        return dp[n][m] ;
+        return dp[N][M];
     }
 }
 
@@ -35,14 +42,26 @@ class Solution {
 
 
 
-
-
-
-
-
-
-
-
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int [][] dp = new int[n][m];
+//         for(int []d:dp)Arrays.fill(d,-1);
+//         return helper(dp , n-1, m-1);
+//     }
+//     public int helper(int [][]dp  , int n , int m){
+//         if(n==0 && m==0){
+//             return  1;
+//         }
+     
+//         if(n<0 || m<0 || n>=dp.length || m>=dp[0].length){
+//             return 0;
+//         }
+//         if(dp[n][m]!=-1)return dp[n][m];
+        
+//          dp[n][m]= helper(dp , n-1 , m)+helper(dp , n ,m-1);
+//         return dp[n][m] ;
+//     }
+// }
 
 
 // class Solution {
